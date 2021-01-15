1e2c0247b8e90d3bee203bc49e5013e4b0d135c7
Francesco <francescoallera@gmail.com>
2021-01-15 15:05:16 +0100

    Setted timeout + array from prompt of numbers

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+// FUNZIONE PER L'ARRAY DI NUMERI CASUALI DIVERSI
 function arrayOfUniqueRandomNumbers(array, arrayLength, minNumberRange, maxNumberRange) {
   while (array.length < arrayLength) {
     var randomNumber = Math.floor(Math.random() * (maxNumberRange + 1 - minNumberRange) + minNumberRange);
@@ -10,8 +11,22 @@ function arrayOfUniqueRandomNumbers(array, arrayLength, minNumberRange, maxNumbe
   })
 }
 
+// VARIABILI
+var numeriDelGioco = 5;
+var secondiDaAspettare = 30;
 var arrayDiNumeriDaRicordare = [];
+var numeriUtente = [];
 
-arrayOfUniqueRandomNumbers(arrayDiNumeriDaRicordare, 5, 1, 100);
+// EFFETTIVO LANCIO DEL GIOCO
+arrayOfUniqueRandomNumbers(arrayDiNumeriDaRicordare, numeriDelGioco, 1, 100);
 
 alert('I numeri che devi ricordare sono ' + arrayDiNumeriDaRicordare + '\nHai 30 secondi per ricordarli!');
+
+setTimeout(function() {
+  for (var i = 0; i < numeriDelGioco; i++) {
+    numeriUtente.push(parseInt(prompt('Scrivi un numero che ti ricordi')));
+  }
+  console.log(numeriUtente);
+}, secondiDaAspettare * 1000);
+
+console.log(arrayDiNumeriDaRicordare);
