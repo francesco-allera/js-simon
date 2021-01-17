42d22080f4a188ad9845c6530172d64c12391113
Francesco <francescoallera@gmail.com>
2021-01-17 18:21:31 +0100

    Renamed variables and functions + Added new messages + emotes

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
-// FUNZIONE PER L'ARRAY DI NUMERI CASUALI DIVERSI
-function arrayOfUniqueRandomNumbers(array, arrayLength, minNumberRange, maxNumberRange) {
+// FUNZIONE PER L'ARRAY DI NUMERI CASUALI DIVERSI ED IN ORDINE CRESCENTE
+function arrayOfUniqueRandomNumbersOrdered(array, arrayLength, minNumberRange, maxNumberRange) {
   while (array.length < arrayLength) {
     var randomNumber = Math.floor(Math.random() * (maxNumberRange + 1 - minNumberRange) + minNumberRange);
     if(!array.includes(randomNumber)) {
@@ -11,7 +11,7 @@ function arrayOfUniqueRandomNumbers(array, arrayLength, minNumberRange, maxNumbe
   })
 }
 // FUNZIONE PER LA VALIDAZIONE DEL NUMERO INSERITO DALL'UTENTE
-function validatorRealNumber(number, minRange, maxRange) {
+function isAValidNumber(number, minRange, maxRange) {
   if (isNaN(number)) {
     return false;
   }
@@ -25,45 +25,73 @@ function validatorRealNumber(number, minRange, maxRange) {
 }
 
 // VARIABILI
-var numeriDelGioco = 5;
 var secondiDaAspettare = 5;
+var numeriDelGioco = 5;
 var numeriMinimi = 1;
 var numeriMassimi = 100;
-var arrayDiNumeriDaRicordare = [];
-var numeriUtente = [];
-var numeriRicordati = [];
+var arrayNumeriDaRicordare = [];
+var arrayNumeriUtente = [];
+var arrayNumeriRicordati = [];
 
 // EFFETTIVO LANCIO DEL GIOCO
-arrayOfUniqueRandomNumbers(arrayDiNumeriDaRicordare, numeriDelGioco, numeriMinimi, numeriMassimi);
+arrayOfUniqueRandomNumbersOrdered(arrayNumeriDaRicordare, numeriDelGioco, numeriMinimi, numeriMassimi);
 
-alert('I numeri che devi ricordare sono ' + arrayDiNumeriDaRicordare + '\nHai ' + secondiDaAspettare + ' secondi per vedere se riesci a ricordarli!');
+alert('I numeri che devi ricordare sono ' + arrayNumeriDaRicordare + '\nHai ' + secondiDaAspettare + ' secondi per vedere se riesci a ricordarli!');
 
 var countdown = setInterval(function() {
   secondiDaAspettare--;
-  document.getElementById('counter').innerText = 'Mancano ' + secondiDaAspettare + ' secondi';
+  if (secondiDaAspettare === 1) {
+    document.getElementById('counter').innerText = 'Mancano: ' + secondiDaAspettare + ' secondo';
+  } else {
+    document.getElementById('counter').innerText = 'Mancano: ' + secondiDaAspettare + ' secondi';
+  }
   if (secondiDaAspettare === 0) {
     clearInterval(countdown);
   }
 }, 1000);
 
-
 setTimeout(function() {
-  while (numeriUtente.length < numeriDelGioco) {
-    var numeroInserito = parseInt(prompt('Scrivi il ' + (numeriUtente.length + 1) + ' numero che ti ricordi'));
-    if (numeriUtente.includes(numeroInserito)) {
-      alert('Hai già inserito questo numero');
-    } else if (!validatorRealNumber(numeroInserito, numeriMinimi, numeriMassimi)){
+  while (arrayNumeriUtente.length < numeriDelGioco) {
+    var numeroInserito = parseInt(prompt('Scrivi il ' + (arrayNumeriUtente.length + 1) + '° numero che ti ricordi'));
+    if (arrayNumeriUtente.includes(numeroInserito)) {
+      alert('Hai già inserito questo numero. Riprova');
+    } else if (!isAValidNumber(numeroInserito, numeriMinimi, numeriMassimi)){
       alert('Inserisci solo numeri compresi tra ' + numeriMinimi + ' e ' + numeriMassimi);
     } else {
-      numeriUtente.push(numeroInserito);
-      if (arrayDiNumeriDaRicordare.includes(numeroInserito)) {
-        numeriRicordati.push(numeroInserito);
+      arrayNumeriUtente.push(numeroInserito);
+      if (arrayNumeriDaRicordare.includes(numeroInserito)) {
+        arrayNumeriRicordati.push(' ' + numeroInserito);
       }
     }
   }
-  console.log(numeriUtente);
-  console.log(numeriRicordati);
-  alert('Hai ricordato ' + numeriRicordati.length + ' numeri\nCioè: ' + numeriRicordati);
-}, secondiDaAspettare * 1000);
+  console.log(arrayNumeriUtente);
+  console.log(arrayNumeriRicordati);
+  switch (arrayNumeriRicordati.length) {
+    case 1:
+      alert('Hai ricordato ' + arrayNumeriRicordati.length + ' numero.\nCioè:' + arrayNumeriRicordati + '. Puoi fare di meglio! ' + String.fromCodePoint(0x1F609));
+    break;
+    case 0:
+      alert('Non hai ricordato nessun numero.\nSei \'na pippa ar sugo! ' + String.fromCodePoint(0x1F923));
+    break;
+    case arrayNumeriDaRicordare.length:
+      alert('Hai ricordato tutti i numeri.\nBig brain guy here! ' + String.fromCodePoint(0x1F60E));
+    break;
+    default:
+      alert('Hai ricordato ' + arrayNumeriRicordati.length + ' numeri.\nCioè:' + arrayNumeriRicordati + ' ' + String.fromCodePoint(0x1F44D));
+  }
+  /* if (arrayNumeriRicordati.length === 1) {
+    var emojiWinking = String.fromCodePoint(0x1F609);
+    alert('Hai ricordato ' + arrayNumeriRicordati.length + ' numero.\nCioè:' + arrayNumeriRicordati + '. Puoi fare di meglio! ' + emojiWinking);
+  } else if (arrayNumeriRicordati.length === 0) {
+    var emojiLaughRolling = String.fromCodePoint(0x1F923);
+    alert('Non hai ricordato nessun numero.\nSei \'na pippa ar sugo! ' + emojiLaughRolling);
+  } else if (arrayNumeriRicordati.length === arrayNumeriDaRicordare.length) {
+    var emojiNerd = String.fromCodePoint(0x1F60E);
+    alert('Hai ricordato tutti i numeri.\nBig brain guy here! ' + emojiNerd);
+  } else {
+    var emojiThumbUp = String.fromCodePoint(0x1F44D);
+    alert('Hai ricordato ' + arrayNumeriRicordati.length + ' numeri.\nCioè:' + arrayNumeriRicordati + ' ' + emojiThumbUp);
+  } */
+}, secondiDaAspettare * 1000 + 200);
 
-console.log(arrayDiNumeriDaRicordare);
+console.log(arrayNumeriDaRicordare);
